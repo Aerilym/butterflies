f2ab55cb27d98707469a5c70b6d476d39800ff85
Aerilym <aerilymbiz@gmail.com>
2022-09-29 18:28:51 +1000

    Add git hook

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -38,6 +38,8 @@ async function handleFetch({ request, env, ctx }: FetchPayload): Promise<Respons
     supabaseKey: env.SB_KEY,
   };
 
+  const tmp = 2;
+
   const body: AuthBody = await request.json();
 
   const supabase = createClient(config.supabaseUrl, config.supabaseKey);
