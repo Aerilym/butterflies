4ed8d2255e748c88798c18fc7aff3349a69bd3f5
Aerilym <aerilymbiz@gmail.com>
2022-10-19 13:51:37 +1100

    Create swipe page

--- packages/wap/src/screens/main/Swipe.tsx
+++ packages/wap/src/screens/main/Swipe.tsx
@@ -1,27 +1,37 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import type { MainStackParamList } from '../../types/navigation';
+import { AuthContext, supabaseAPI } from '../../provider/AuthProvider';
+import { Match } from '../../types/database';
+import { SwipeCard } from '../../components/swipe/SwipeCard';
 
 export default function ({ navigation }: NativeStackScreenProps<MainStackParamList, 'Swipe'>) {
+  const { session } = useContext(AuthContext);
+  const userID = session?.user.id ?? '';
+
+  const [matchQueue, setMatchQueue] = useState<null | Match[]>(null);
+
+  useEffect(() => {
+    supabaseAPI.getMatchQueue(userID).then((matches) => {
+      setMatchQueue(matches);
+    });
+  }, []);
   return (
     <View
       style={{
         flex: 1,
-        alignItems: 'center',
         justifyContent: 'center',
       }}
     >
-      <Text style={{ textAlign: 'center' }}>
-        No profiles match your filters, try expanding them!
-      </Text>
-      <Button
-        title="Expand filters"
-        onPress={() => {
-          navigation.navigate('Filters');
-        }}
-      />
+      {matchQueue ? (
+        <SwipeCard navigation={navigation} userID={userID} match={matchQueue[0]} />
+      ) : (
+        <Text style={{ textAlign: 'center' }}>
+          No profiles match your filters, try expanding them!
+        </Text>
+      )}
     </View>
   );
 }
