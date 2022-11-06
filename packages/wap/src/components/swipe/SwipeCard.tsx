6d3707300a91faf0a7dead3591899ea94f1300d5
Aerilym <aerilymbiz@gmail.com>
2022-11-06 18:45:47 +1100

    Remove unused imports

--- packages/wap/src/components/swipe/SwipeCard.tsx
+++ packages/wap/src/components/swipe/SwipeCard.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react';
 import { TouchableOpacity, Text, View, ImageBackground } from 'react-native';
 
 import { MatchQueueItem } from '../../types/social';
