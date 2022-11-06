f85905ddaa6ccb96f97c4b61ab8e10da025ba0fc
Aerilym <aerilymbiz@gmail.com>
2022-11-07 01:35:49 +1100

    Add comments to user store

--- packages/wap/src/api/UserStore.ts
+++ packages/wap/src/api/UserStore.ts
@@ -12,6 +12,9 @@ export class UserStore {
     this.getMatchQueue();
   }
 
+  /**
+   * Get the socials item from the async storage and assigns it to the socials property. If there isn't anything in the async storage, it will fetch the socials from the database.
+   */
   async getSocials(): Promise<void> {
     const storedSocials = await this.getItem('@socials');
     if (storedSocials) {
@@ -23,6 +26,9 @@ export class UserStore {
     }
   }
 
+  /**
+   * Get the match queue item from the async storage and assigns it to the matchQueue property. If there isn't anything in the async storage, it will fetch the match queue from the database.
+   */
   async getMatchQueue(): Promise<void> {
     const storedMatchQueue = await this.getItem('@matchQueue');
     if (storedMatchQueue) {
@@ -34,26 +40,46 @@ export class UserStore {
     }
   }
 
+  /**
+   * Refresh the socials property from supabase and store it in the socials property and the async storage.
+   */
   refreshSocials = async (): Promise<void> => {
     const matches = await supabaseAPI.getMatches();
     this.socials = await this.getPeople(matches, true);
     await this.storeSocials(this.socials);
   };
 
+  /**
+   * Refresh the match queue property from supabase and store it in the matchQueue property and the async storage.
+   */
   refreshMatchQueue = async (): Promise<void> => {
     const matches = await supabaseAPI.getMatchQueue();
     this.matchQueue = await this.getPeople(matches, false);
     await this.storeMatchQueue(this.matchQueue);
   };
 
+  /**
+   * Store the socials property in the async storage.
+   * @param socials The socials to store in the async storage
+   */
   storeSocials = async (socials: Person[]): Promise<void> => {
     await this.storeItem('@socials', socials);
   };
 
+  /**
+   * Store the match queue property in the async storage.
+   * @param matchQueue The match queue to store in the async storage
+   */
   storeMatchQueue = async (matchQueue: Person[]): Promise<void> => {
     await this.storeItem('@matchQueue', matchQueue);
   };
 
+  /**
+   * Store an item in the async storage.
+   * @param key The key to store the item under
+   * @param item The item to store
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   storeItem = async (key: string, item: any): Promise<void> => {
     try {
       await AsyncStorage.setItem(key, JSON.stringify(item));
@@ -62,6 +88,12 @@ export class UserStore {
     }
   };
 
+  /**
+   * Get an item from the async storage.
+   * @param key The key to get the item from
+   * @returns The item stored under the key
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   getItem = async (key: string): Promise<any> => {
     try {
       const item = await AsyncStorage.getItem(key);
@@ -71,6 +103,12 @@ export class UserStore {
     }
   };
 
+  /**
+   * Get the people social profiles from the matches from supabase.
+   * @param matches The matches to get the people social profiles from.
+   * @param matched Whether the matches are matched or not.
+   * @returns The people from the matches.
+   */
   getPeople = async (matches: Match[], matched: boolean): Promise<Person[]> => {
     const peopleGets = matches.map(async (match) => {
       const userID = match.user_id1 === supabaseAPI.userID ? match.user_id2 : match.user_id1;
@@ -90,6 +128,11 @@ export class UserStore {
     return people;
   };
 
+  /**
+   * Add a message to the messages property of the person with the given match id.
+   * @param matchID The match id to add the message to
+   * @param message The message to add to the messages
+   */
   addMessage = async (matchID: string, message: Message): Promise<void> => {
     if (!this.socials) return;
     const social = this.socials.find((social) => social.match.match_id === matchID);
