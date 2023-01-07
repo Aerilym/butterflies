7f9ded5c8e9c960fe5c0cd6088e04b6402852d24
Aerilym <aerilymbiz@gmail.com>
2023-01-07 19:00:50 +0900

    Create location data manager in user store

--- packages/wap/src/screens/settings/Settings.tsx
+++ packages/wap/src/screens/settings/Settings.tsx
@@ -1,7 +1,9 @@
 import { Box, Text, Button } from 'native-base';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { formatDistanceToNow } from 'date-fns';
 
 import type { MainStackParamList } from '../../types/navigation';
+import { userStore } from '../../provider/AuthProvider';
 
 export default function ({ navigation }: NativeStackScreenProps<MainStackParamList, 'Settings'>) {
   return (
@@ -20,6 +22,45 @@ export default function ({ navigation }: NativeStackScreenProps<MainStackParamLi
       >
         <Text>Return</Text>
       </Button>
+      {userStore.locationData ? (
+        <Box>
+          <Text>
+            {userStore.locationData.coords.latitude}, {userStore.locationData.coords.longitude}
+          </Text>
+
+          <Text>Last Updated: {formatDistanceToNow(userStore.locationData.timestamp)}</Text>
+
+          <Text>Accuracy: {userStore.locationData.coords.accuracy}</Text>
+
+          <Text>Altitude: {userStore.locationData.coords.altitude}</Text>
+
+          <Text>Altitude Accuracy: {userStore.locationData.coords.altitudeAccuracy}</Text>
+
+          <Text>Heading: {userStore.locationData.coords.heading}</Text>
+
+          <Text>Speed: {userStore.locationData.coords.speed}</Text>
+
+          <Text>Mocked: {userStore.locationData.mocked ? 'Yes' : 'No'}</Text>
+
+          <Text>Name: {userStore.locationData.name}</Text>
+
+          <Text>
+            Address:
+            {[
+              userStore.locationData.streetNumber + ' ' + userStore.locationData.street,
+              userStore.locationData.district,
+              userStore.locationData.city,
+              userStore.locationData.subregion,
+              userStore.locationData.region,
+              userStore.locationData.country,
+            ]
+              .map((line) => line?.replaceAll('undefined', ''))
+              .filter((line) => line)
+              .filter((line) => line !== ' ')
+              .join(', ')}
+          </Text>
+        </Box>
+      ) : null}
     </Box>
   );
 }
