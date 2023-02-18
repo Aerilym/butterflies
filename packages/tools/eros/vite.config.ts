f2af5d93e8e931ee906c6d961ec9d5764a5b1b50
Aerilym <aerilymbiz@gmail.com>
2023-02-18 12:22:07 +0700

    Add linter

--- packages/tools/eros/vite.config.ts
+++ packages/tools/eros/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      // default settings on build (i.e. fail on error)
+      ...eslint(),
+      apply: 'build',
+    },
+    {
+      // do not fail on serve (i.e. local development)
+      ...eslint({
+        failOnWarning: false,
+        failOnError: false,
+      }),
+      apply: 'serve',
+      enforce: 'post',
+    },
+  ],
+});
